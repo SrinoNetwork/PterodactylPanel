7a6993ccd6aa2954ff2bb8cfe442a4a5d8f8f637
kamiql <kilianaqua08@gmail.com>
2026-03-24 21:57:20 +0100

    fix: simplify server access logic in ClientController

--- app/Http/Controllers/Api/Client/ClientController.php
+++ app/Http/Controllers/Api/Client/ClientController.php
@@ -41,14 +41,9 @@ class ClientController extends ClientApiController
         ]);
 
         $type = $request->input('type');
-        // Either return all the servers the user has access to because they are an admin `?type=admin` or
-        // just return all the servers the user has access to because they are the owner or a subuser of the
-        // server. If ?type=admin-all is passed all servers on the system will be returned to the user, rather
-        // than only servers they can see because they are an admin.
+
         if (in_array($type, ['admin', 'admin-all'])) {
-            // If they aren't an admin but want all the admin servers don't fail the request, just
-            // make it a query that will never return any results back.
-            if (!$user->root_admin && !$user->access_all_servers) {
+            if (!$user->root_admin) {
                 $builder->whereRaw('1 = 2');
             } else {
                 $builder = $type === 'admin-all'
@@ -58,7 +53,7 @@ class ClientController extends ClientApiController
         } elseif ($type === 'owner') {
             $builder = $builder->where('servers.owner_id', $user->id);
         } else {
-            if (!$user->root_admin && !$user->access_all_servers) {
+            if (!$user->root_admin) {
                 $builder = $builder->whereIn('servers.id', $user->accessibleServers()->pluck('id')->all());
             }
         }
