f2b626fd0af19ffe9e31b914eaddaa44a183ea87
kamiql <kilianaqua08@gmail.com>
2026-03-24 23:11:13 +0100

    fix: refine server access logic in ClientController for improved admin query handling

--- app/Http/Controllers/Api/Client/ClientController.php
+++ app/Http/Controllers/Api/Client/ClientController.php
@@ -29,7 +29,6 @@ class ClientController extends ClientApiController
         $user = $request->user();
         $transformer = $this->getTransformer(ServerTransformer::class);
 
-        // Start the query builder and ensure we eager load any requested relationships from the request.
         $builder = QueryBuilder::for(
             Server::query()->with($this->getIncludesForTransformer($transformer, ['node']))
         )->allowedFilters([
@@ -41,24 +40,21 @@ class ClientController extends ClientApiController
         ]);
 
         $type = $request->input('type');
-        // Either return all the servers the user has access to because they are an admin `?type=admin` or
-        // just return all the servers the user has access to because they are the owner or a subuser of the
-        // server. If ?type=admin-all is passed all servers on the system will be returned to the user, rather
-        // than only servers they can see because they are an admin.
+
         if (in_array($type, ['admin', 'admin-all'])) {
-            // If they aren't an admin but want all the admin servers don't fail the request, just
-            // make it a query that will never return any results back.
             if (!$user->root_admin && !$user->access_all_servers) {
-                $builder->whereRaw('1 = 2');
-            } else {
-                $builder = $type === 'admin-all'
-                    ? $builder
-                    : $builder->whereNotIn('servers.id', $user->accessibleServers()->pluck('id')->all());
+                $builder->whereRaw('1 = 2'); // kein Zugriff
+            }
+            // für admin-all oder access_all_servers lassen wir die Query unfiltered
+            elseif ($type === 'admin') {
+                if (!$user->access_all_servers) {
+                    $builder->whereNotIn('servers.id', $user->accessibleServers()->pluck('id')->all());
+                }
             }
         } elseif ($type === 'owner') {
-            $builder = $builder->where('servers.owner_id', $user->id);
+            $builder->where('servers.owner_id', $user->id);
         } else {
-            $builder = $builder->whereIn('servers.id', $user->accessibleServers()->pluck('id')->all());
+            $builder->whereIn('servers.id', $user->accessibleServers()->pluck('id')->all());
         }
 
         $servers = $builder->paginate(min($request->query('per_page', 50), 100))->appends($request->query());
